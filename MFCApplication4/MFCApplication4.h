bfcf2cfd435e36a412663e4242d2cfad4c255bbf
iyzyi <kljxn@qq.com>
2021-02-06 13:34:15 +0800

    添加项目文件。

---
+++ MFCApplication4/MFCApplication4.h
@@ -0,0 +1,32 @@
+﻿
+// MFCApplication4.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCApplication4App:
+// 有关此类的实现，请参阅 MFCApplication4.cpp
+//
+
+class CMFCApplication4App : public CWinApp
+{
+public:
+	CMFCApplication4App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication4App theApp;
