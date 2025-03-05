ebf9389e95a22d5b5caa64b72ea3c2ff30b6c0dc
iyzyi <kljxn@qq.com>
2025-03-05 16:49:46 +0800

    Add bat to clean dev env

--- MuaClient/clean.bat
+++ MuaClient/clean.bat
@@ -1,3 +1,3 @@
-rd /S "C:\Users\iyzyi\AppData\Roaming\Windows Defender"
-del /a "C:\Users\iyzyi\AppData\Local\Temp\{FDA3820B-5558-4849-A085-D07257C2775F}"
-del /a "C:\Users\iyzyi\AppData\Local\Temp\{00E92116-51A2-4538-8228-03EA82922FC2}"
+rd /S "%USERPROFILE%\AppData\Roaming\Windows Defender"
+del /a "%USERPROFILE%\AppData\Local\Temp\{FDA3820B-5558-4849-A085-D07257C2775F}"
+del /a "%USERPROFILE%\AppData\Local\Temp\{00E92116-51A2-4538-8228-03EA82922FC2}"
